d00eb90b0ab2852cb303f0ffb982b76010f2a75e
inazr <github@lentzen.email>
2020-02-21 18:22:45 +0100

    cleaned

--- run_me.py
+++ run_me.py
@@ -1,14 +1,78 @@
+'''
+v.0.0.1
+Allgemeine Informationen:
+Bei Fragen: Stephan@BI
+Die Berechnungen funktionieren bei Ehegattensplitting (noch) nicht.
+Alle Angaben ohne Gewähr!
+'''
+
 import pandas as pd
 from datetime import datetime
 from datetime import timedelta
-from personal_info import person
 import math
 
+'''
+In diesem Abschnitt bitte Änderungen vornehmen:
+Alle Angaben p.a. !
+'''
+Geburtsdatum = datetime.strptime('14.11.1986', '%d.%m.%Y')
+'''
+    IdR: 01. des Monats nach dem 67. Geburtstag
+    Mit Abzug: frühestens nach dem 63. Geburstag
+    Mit GdB von >= 50: frühestens nach dem 62. Geburstag
+'''
+Renteneintritt = datetime.strptime('01.12.2053', '%d.%m.%Y')
+
+BAV_Vertragsbeginn = datetime.strptime('01.04.2020', '%d.%m.%Y')
+
+Bruttoeinkommen = 48000
+Gehaltssteigerung_e = 0.02
+
+BAV_Bruttobeitrag = 2400
+BAV_ST_Zuschuss = 0.15
+
+Freistellungsauftrag: 801   # Verheiratet: max 1.602 €, Alleinstehend: max. 801 €
+Kapitalmarktzins_e = 0.05
+
+# Krankenversicherung
+GKV: True
+GKV_Zusatzbeitrag = 0.07
+PKV: False
+PKV_Beitrag: 0
+
+# Sonstige Angaben
+Kinder: False
+Kirche: False
+
+
+'''
+In diesem Abschnitt bitte nur Änderungen vornehmen, wenn du weißt was du tust:
+'''
+BAV_Bruttoeinkommen = Bruttoeinkommen - BAV_Bruttobeitrag
+
+# Beitragssätze 2020:
 GKV_Beitragssatz = 0.146
 AV_Beitragssatz = 0.024
 RV_Beitragssatz = 0.186
 PV_Beitragssatz = 0.0305
 Kinderlosenmalus = 0.0025
+Solidaritaetszuschlag = 0.055
+Kirchensteuer = 0.09
+
+# Beitragsbemessungsgrenzen 2020:
+GKV_Beitragsbemessungsgrenze = 56250
+PV_Beitragsbemessungsgrenze = 56250
+RV_Beitragsbemessungsgrenze = 82800
+AV_Beitragsbemessungsgrenze = 82800
+
+# Entwicklung durchschnittsentgelt
+durchschnittsentgelt_rentenversicherung_2020 = 40551
+durchschnittsentgelt_rentenversicherung = 0.0185
+
+
+'''
+In diesem Abschnitt bitte - KEINE - Änderungen vornehmen:
+'''
 
 def calc_steuern_sozialabgaben(Bruttoeinkommen, GKV, GKV_Zusatzbeitrag, PKV, PKV_Beitrag, Kinder, Kirche):
 
@@ -89,7 +153,22 @@ def calc_steuern_sozialabgaben(Bruttoeinkommen, GKV, GKV_Zusatzbeitrag, PKV, PKV
     return KV, AV, PV, RV, zvE, Lohnsteuer, Soli, Kirchensteuer, Nettoeinkommen
 
 
+
+
+def calc_Verlust_Rentenpunkte(Bruttoeinkommen):
+
+
 if __name__ == "__main__":
     print("YOUR MILEAGE MY VARY!")
 
-    KV, AV, PV, RV, zvE, Lohnsteuer, Soli, Kirchensteuer, Nettoeinkommen = calc_steuern_sozialabgaben(person['Bruttoeinkommen'], person['GKV'], person['GKV Zusatzbeitrag'], person['PKV'], person['PKV Beitrag'], person['Kinder'], person['Kirchensteuer'])
+    KV, AV, PV, RV, zvE, Lohnsteuer, Soli, Kirchensteuer, Nettoeinkommen = calc_steuern_sozialabgaben(
+        Bruttoeinkommen, person['GKV'], person['GKV Zusatzbeitrag'], person['PKV'], person['PKV Beitrag'],
+        person['Kinder'], person['Kirchensteuer'])
+
+
+    _, _, _, _, _, _, _, _, Nettoeinkommen_BAV = calc_steuern_sozialabgaben(
+        Bruttoeinkommen_BAV, person['GKV'], person['GKV Zusatzbeitrag'], person['PKV'], person['PKV Beitrag'],
+        person['Kinder'], person['Kirchensteuer'])
+
+    BAV_Monatsbeitrag_Netto  = round((Nettoeinkommen - Nettoeinkommen_BAV )/ 12, 2)
+    print(BAV_Monatsbeitrag_Netto)
